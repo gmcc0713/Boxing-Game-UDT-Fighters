f20fd60ac7bb08810077e27c387c5094832f4b74
geunyong12 <59678097+gmcc0713@users.noreply.github.com>
2023-11-06 16:50:53 +0900

    11.06 이미지 애니메이션
    ready fight 애니메이션

--- BoxingGame/Assets/_Script/Text/TextImageEffectManager.cs
+++ BoxingGame/Assets/_Script/Text/TextImageEffectManager.cs
@@ -1,34 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
 
 public enum EffectImage
 {
-    Ready=0,
-    Fight,
-    KO,
-    Count,
+	Ready = 0,
+	Fight,
+	KO,
+	Count,
 }
 public class TextImageEffectManager : MonoBehaviour
 {
 	public Image[] effectImages;
 	public TextImageEffecter[] effecters;
 	void Start()
-    {
+	{
 		Debug.Log("Start");
 		StartCoroutine(GameStartReadyFightImageAnimation());
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
 	}
-    IEnumerator GameStartReadyFightImageAnimation()
-    {
+	IEnumerator GameStartReadyFightImageAnimation()
+	{
 		Debug.Log("Start");
-		effecters[(int)EffectImage.Ready].StartSizeUp();
+		effecters[(int)EffectImage.Ready].gameObject.SetActive(true);
 		yield return new WaitForSeconds(3.0f);
 	}
 
