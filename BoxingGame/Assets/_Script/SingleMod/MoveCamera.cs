e2e0f6d88213c2293e54e9283618a0bddccb1bad
minwn3318 <123531300+minwn3318@users.noreply.github.com>
2023-11-16 20:56:55 +0900

    연습씬 카메라 움직임 구현

--- BoxingGame/Assets/_Script/SingleMod/MoveCamera.cs
+++ BoxingGame/Assets/_Script/SingleMod/MoveCamera.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     public GameObject player;
-    Vector3 cameraPoint = new Vector3(0, 3, -6);
+    Vector3 cameraPoint = new Vector3(-0.18f, 10f, -20f);
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,6 @@ public class MoveCamera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-       transform.position = player.transform.position + cameraPoint;
+        transform.position = player.transform.position + cameraPoint;
     }
 }
